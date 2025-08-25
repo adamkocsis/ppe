5443249cfd489e294c8cd12fa01bb1573213b3ad
adamkocsis <adam.t.kocsis@gmail.com>
2025-08-25 16:22:05 +0200

    Initial.

---
+++ code/figure_comparison.R
@@ -0,0 +1,7 @@
+library(rgplates)
+library(chronosphere)
+
+setwd("/mnt/sky/Dropbox/WorkSpace/2025-06-10_PPE/")
+
+# get the TorsvikCocks model (rgplates::platemodel class object)
+source("code/methods/TorsvikCocks2025.R")
