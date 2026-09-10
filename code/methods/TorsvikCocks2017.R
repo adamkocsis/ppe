c478a9d03b244beaf02208064b2b4f330b2f3e64
adamkocsis <adam.t.kocsis@gmail.com>
2026-09-10 19:46:31 +0200

    Deleted unneeded stuff.

--- code/methods/TorsvikCocks2017.R
+++
@@ -1,9 +0,0 @@
-
-# where they are
-dir <- "data/TorsvikCocks2017"
-
-# features
-features<- c("static_polygons"=file.path(dir,"Torsvik_Cocks_2016_Terranes.gpml"))
-
-# The model
-TC2017 <- platemodel(rotation=file.path(dir,"Torsvik_Cocks_HybridRotationFile.rot"), features=features)
